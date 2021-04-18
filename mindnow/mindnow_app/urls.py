d18a150b748040d6e510033d55c5d409d7049972
Drznedaj <cvajblumen@gmail.com>
2021-04-18 21:11:21 +0200

    Add new urls to the list

--- mindnow/mindnow_app/urls.py
+++ mindnow/mindnow_app/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
     path("redirect_func/<int:id>", views.redirect_func, name="redirect_func"),
+    path("create/", views.create_url, name="create"),
+    path("delete_link/<int:id>", views.delete_link, name="delete_link"),
+    path("edit_url/<int:id>", views.edit_url, name="edit_url"),
+    path("edit_link/<int:id>", views.edit_link, name="edit_link"),
 ]
