078bb5684db4dfa1b0ed03e7f10f8cf8f5342d9a
Drznedaj <cvajblumen@gmail.com>
2021-04-18 21:01:23 +0200

    Add forms

--- mindnow/mindnow_app/forms.py
+++ mindnow/mindnow_app/forms.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import ShortLink, Link
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class ShortLinkForm(ModelForm):
+    class Meta:
+        model = ShortLink
+        fields = ["short_url_id"]
+
+
+class LinkForm(ModelForm):
+    class Meta:
+        model = Link
+        exclude = ["link"]
